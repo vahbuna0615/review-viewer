d549e1a9daf1274197efd716f974447dbf065fa6
anubhav <anubhavadhikari15@gmail.com>
2023-03-26 12:16:42 +0530

    commit-1

---
+++ app.js
@@ -0,0 +1,87 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    img: 'https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+//select items
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randBtn = document.querySelector('.random-btn');
+
+//set starting item
+let count = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function(){
+  showPerson();
+});
+
+//show person based on item
+
+function showPerson(){
+  const item = reviews[count];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+};
+
+//show next person
+
+nextBtn.addEventListener('click', function(){
+  count++;
+  if (count > reviews.length - 1){
+    count = 0;
+  }
+  showPerson();
+});
+
+//show prev person
+
+prevBtn.addEventListener('click', function(){
+  count--;
+  if (count < 0){
+    count = reviews.length - 1 ;
+  }
+  showPerson();
+});
+
+//show random person
+
+randBtn.addEventListener('click', function(){
+  count = Math.floor(Math.random() * reviews.length);
+  showPerson();
+})
